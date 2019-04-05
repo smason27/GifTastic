be6ac7e21ec04051b7e8f79a85f17ba76cec7286
smason27 <masonsteven27@gmail.com>
2019-04-04 23:41:56 -0700

    homework submission

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -7,7 +7,7 @@
 
 $(document).ready(function () {
     var categories = ["racoons", "dogs", "golf", "snowboarding", "bears", "the office", "basketball", "video games", "giraffes"]
-    var gifAnimate;
+
     function createButton() {
         for (i = 0; i < categories.length; i++) {
             var button = $("<button>");
@@ -20,55 +20,71 @@ $(document).ready(function () {
     $("#addGif").on("click", function () {
         event.preventDefault();
         var newCategory = $("#gifInput").val();
+       
         categories.push(newCategory);
         $("#generatedButton").empty();
         createButton();
-        console.log(categories)
     })
 
     $(document).on("click", ".categoryButton", function () {
-        $("#gifs").empty();
-        var apiKey = "&limit=10&api_key=xXacPubhr3fC6JSlSV5faFaG1J7i6vf2"
+        var rating = $("#searchRating").val();
+        console.log(rating)
+        var apiKey = "&api_key=xXacPubhr3fC6JSlSV5faFaG1J7i6vf2"
         var queryUrl = "https://api.giphy.com/v1/gifs/search?q=" + 
-        $(this).attr("data-name") + apiKey
+        $(this).attr("data-name") + "&limit=10&" + apiKey + "&rating=" + rating
 
         $.ajax({
             url: queryUrl,
             method: "GET"
         }).then(function (response) {
-            console.log(response);
-            var newGif = $("#gifs")
-            for(let key of response.data) {
-                var rating = key.rating
-                var gifImg = key.images.fixed_height_still.url
-                var gifStill = key.images.fixed_height_still.url
-                gifAnimate = key.images.fixed_height.url
+            $("#leftGifs").empty();
+            $("#rightGifs").empty();
+                for(var i = 0; i < response.data.length; i++){
+                var leftGif = $("#leftGifs")
+                var rightGif = $("#rightGifs")
+                var gifRating = response.data[i].rating
+                // IF STATEMENT TO CHECK RATING IS === RATING SEARCHED BY
+                // if(gifRating !== rating) {
+                // }
+                console.log(gifRating)
+                var gifImg = response.data[i].images.fixed_height_still.url
+                var gifStill = response.data[i].images.fixed_height_still.url
+                var gifAnimate = response.data[i].images.fixed_height.url
                 var gifDiv = $("<img>")
                 gifDiv.attr("src", gifImg)
                 gifDiv.attr("data-state", "still").attr("data-still", gifStill).attr("data-animate", gifAnimate)
                 gifDiv.addClass("gifImage")
-                console.log(gifImg)
-                console.log(rating)
-                newGif.append("<div class='rating'>" + rating + "</div>")
-                newGif.append(gifDiv)    
-                console.log(gifImg)
+                if(i % 2 === 0){
+                    leftGif.append("<div class='rating'>" + gifRating + "</div>")
+                    leftGif.append(gifDiv)  
+                } else{
+                    rightGif.append("<div class='rating'>" + gifRating + "</div>")
+                    rightGif.append(gifDiv)  
+
+                }
             }
+
+            $(".gifImage").on("click", function() {
+                var state = $(this).attr("data-state");
+                var animateState = $(this).attr("data-animate")
+                var stillState = $(this).attr("data-still")
+        
+                if(state === "still") {
+                    $(this).attr("src", animateState);
+                    $(this).attr("data-state", "animate");
+                } else {
+                    $(this).attr("src", stillState);
+                    $(this).attr("data-state", "still")
+                }
+            })
             
         })
+        
     })
+    
 
-    $("#gifs").on("click", ".gifImage", function() {
-        var state = $(this).attr("data-state");
-
-        if(state === "still") {
-            $(this).attr("src", gifAnimate);
-            $(this).attr("data-state", "animate");
-        } else {
-            $(this).attr("src", "data-still");
-            $(this).attr("data-state", "still")
-        }
-    })
+   
 
     createButton();
 })
-console.log("hello")
+
